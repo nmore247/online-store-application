7e4a11d9dbe2ef5775df86554d8e90c4eedd0633
Nikhil <nikhilmore2011@hotmail.com>
2024-04-08 15:46:54 +0200

    add material modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+const materialModules = [
+  MatCommonModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSidenavModule,
+  MatSlideToggleModule
+
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    materialModules
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
