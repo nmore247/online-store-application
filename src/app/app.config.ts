50e67044fc180a9d12ce6eba8f971d8c2c2e3534
Nikhil <nikhilmore2011@hotmail.com>
2024-05-06 14:04:14 +0200

    add authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true
+    }
   ]
 };
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthenticationService } from '../auth/authentication.service';
+
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot,state: RouterStateSnapshot) => {
+  const authService = inject(AuthenticationService);
+  const router = inject(Router);
+  if (authService.isLoggedIn()) {
+    router.navigate(['/login']);
+  }
+  return true;
+};
