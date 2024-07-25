68b258d5a94c121a732cdd4134e77810df62dab7
Nikhil <nikhilmore2011@hotmail.com>
2024-07-25 11:41:47 +0200

    add product rating stars

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -15,12 +15,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class ProductDetailComponent implements OnInit {
   public products!: IProduct[];
+  public productStars!: number[];
   constructor(
     private productService: ProductListApplicationService,
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private cartService: CartService,
     private snackBar: MatSnackBar
-  ) {}
+  ) { }
 
   public product!: IProduct;
 
@@ -28,7 +29,6 @@ export class ProductDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.productService.getSingleProduct(Number(id));
     this.getCurrentProduct();
-    //this.fetchAllProducts();
   }
 
   public addToCart() {
@@ -39,9 +39,16 @@ export class ProductDetailComponent implements OnInit {
     });
   }
 
-  private getCurrentProduct(){
+  private getCurrentProduct() {
     this.productService._selectedProduct$.subscribe((data) => {
+      if (data) {
       this.product = data;
+        this.productStars = this.getStars(data);
+      }
     });
+  }
+
+  private getStars(product: IProduct) {
+    return Array(Math.floor(product?.rating.rate)).fill(0);
   }
 }
