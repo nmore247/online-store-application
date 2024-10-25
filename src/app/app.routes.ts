49041a8e518fefe46b0bdbcac5ea53059805ba00
Nikhil <nikhilmore2011@hotmail.com>
2024-10-25 09:45:49 +0200

    Revert "implement lazy loading"
    This reverts commit 323b02773e45f63d1cfd1afd61783b0a2f5cf3fe.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './cart/cart/cart.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
-
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', loadComponent: () => import("./home/home.component").then((m) => m.HomeComponent) },
-  { path: 'login', loadComponent: () => import("./authentication/login/login.component").then((m) => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import("./authentication/register/register.component").then((m) => m.RegisterComponent) },
-  { path: 'products/:id', loadComponent: () => import("./products/product-detail/product-detail.component").then((m) => m.ProductDetailComponent) },
-  { path: 'cart', loadComponent: () => import("./cart/cart/cart.component").then((m) => m.CartComponent) }
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent },
 ];
