7102874351224588d89fd5a2a8721aae1df7f791
Nikhil <nikhilmore2011@hotmail.com>
2025-02-07 11:18:43 +0100

    add separate product card component

---
+++ src/app/products/product-card/product-card.component.ts
@@ -0,0 +1,50 @@
+import {Component, computed, inject, Input} from '@angular/core';
+import {ProductsService} from "../products.service";
+import {CartService} from "../../cart/cart.service";
+import {IProduct} from "../product";
+import {MatIcon} from "@angular/material/icon";
+import {CurrencyPipe} from "@angular/common";
+import {MatIconButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+
+@Component({
+  selector: 'app-product-card',
+  imports: [
+    MatIcon,
+    CurrencyPipe,
+    MatIconButton,
+    MatTooltip
+  ],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+  private productsService = inject(ProductsService);
+  private cartService = inject(CartService);
+  errorMessage = '';
+  numbers: number[] = [];
+
+  @Input() product!: IProduct
+
+  constructor() {
+    this.generateNumbers(1, 10);
+  }
+
+  public products = computed(() => {
+    try {
+      return this.productsService.products();
+    } catch (e) {
+      this.errorMessage = typeof e === 'string' ? e : 'Error';
+      return [];
+    }
+  });
+
+  private generateNumbers(start: number, end: number) {
+    this.numbers = Array.from({length: end - start + 1}, (_, i) => start + i);
+  }
+
+  public addToCart(product: IProduct) {
+    this.cartService.addToCart(product)
+  }
+
+}
--- src/app/products/products-container/products-container.component.ts
+++ src/app/products/products-container/products-container.component.ts
@@ -1,20 +1,11 @@
 import {Component, computed, inject} from '@angular/core';
 import {ProductsService} from "../products.service";
-import {CurrencyPipe} from "@angular/common";
-import {MatTooltip} from "@angular/material/tooltip";
-import {CartService} from "../../cart/cart.service";
-import {IProduct} from "../product";
-import {MatIconButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-
+import {ProductCardComponent} from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-products-container',
   imports: [
-    CurrencyPipe,
-    MatTooltip,
-    MatIconButton,
-    MatIcon
+    ProductCardComponent
   ],
   templateUrl: './products-container.component.html',
   styleUrl: './products-container.component.scss'
@@ -22,12 +13,10 @@ import {MatIcon} from "@angular/material/icon";
 export class ProductsContainerComponent {
 
   private productsService = inject(ProductsService);
-  private cartService = inject(CartService);
   errorMessage = '';
-  numbers: number[] = [];
 
   constructor() {
-    this.generateNumbers(1, 10);
+
   }
 
   public products = computed(() => {
@@ -47,21 +36,6 @@ export class ProductsContainerComponent {
       return [];
     }
   });
-
-  private generateNumbers(start: number, end: number) {
-    this.numbers = Array.from({length: end - start + 1}, (_, i) => start + i);
-  }
-
-  public addToCart(product: IProduct) {
-    this.cartService.addToCart(product)
-  }
-
-  public productSelected(productName: string) {
-    if (productName) {
-      this.productsService.productSelected(productName)
-    }
-  }
-
 
 
 
